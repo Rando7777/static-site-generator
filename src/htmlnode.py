2f9501e6425712d9fc20c68efd162f7ee4ac7da3
rando <rando.poolak@gmail.com>
2025-09-20 00:55:37 +0300

    HTMLNode and some tests for that class

---
+++ src/htmlnode.py
@@ -0,0 +1,47 @@
+from typing import List, Optional
+
+class HTMLNode():
+    def __init__(
+            self, 
+            tag: Optional[str] = None, 
+            value: Optional[str] = None, 
+            children: Optional[List['HTMLNode']] = None, 
+            props: Optional[dict] = None
+            ):
+        # Basic validation for expected inputs
+        if tag and not isinstance(tag, str):
+            raise ValueError('Tag has to be instance of string')
+        if value and not isinstance(value, str):
+            raise ValueError('Value has to be instance of string')
+        if children and not all(isinstance(c, HTMLNode) for c in children):
+            raise ValueError('All children must be instane of HTMLNode')
+        if props and not isinstance(props, dict):
+            raise ValueError('Props has to be instance of dict')
+
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplemented('This is not yet implemented')
+
+    def props_to_html(self):
+        if not self.props:
+            return ''
+        if self.props:
+            prop_list = []
+            for k, v in self.props.items():
+                prop_list.append(f'{k}="{v}"')
+        return ' '+ ' '.join(prop_list) + ''
+
+    def __eq__(self, other):
+        return (self.tag == other.tag and
+                self.value == other.value and
+                self.children == other.children and
+                self.props == other.props)
+
+    def __repr__(self):
+        return f'HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})'
+
+
---
+++ src/test_htmlnode.py
@@ -0,0 +1,54 @@
+from htmlnode import HTMLNode
+import unittest
+
+class TestHTMLNode(unittest.TestCase):
+    node1 = HTMLNode()
+    node2 = HTMLNode()
+
+    def test_eq1(self):
+        self.assertEqual(self.node1, self.node2)
+
+    def test_eq2(self):
+        node3 = HTMLNode(value="This is node with children", children=[self.node1, self.node2])
+        node4 = HTMLNode(value="This is node with children", children=[self.node1, self.node2])
+        self.assertEqual(node3, node4)
+
+    def test_eq3(self):
+        node3 = HTMLNode(tag='Tag String', value='Value String', children=[self.node1], props={'href': 'https://www.google.com', 'target': '_blank'})
+        node4 = HTMLNode(tag='Tag String', value='Value String', children=[self.node1], props={'href': 'https://www.google.com', 'target': '_blank'})
+        self.assertEqual(node3, node4)
+
+    def test_not_eq1(self):
+        node3 = HTMLNode(value="This is node with value")
+        node4 = HTMLNode(children=[self.node1, self.node2])
+        self.assertNotEqual(node3, node4)
+
+    def test_not_eq2(self):
+        node3 = HTMLNode(value="This is same", props={'not': 'same'})
+        node4 = HTMLNode(value="This is same", props={'same': 'not'})
+        self.assertNotEqual(node3, node4)
+
+    def test_invalid_values1(self):
+        self.assertRaises(ValueError, HTMLNode, tag=1)
+
+    def test_invalid_values2(self):
+        self.assertRaises(ValueError, HTMLNode, value=True)
+
+    def test_invalid_values3(self):
+        self.assertRaises(ValueError, HTMLNode, children=[1, True, "test"])
+
+    def test_invalid_values4(self):
+        self.assertRaises(ValueError, HTMLNode, props=1)
+
+    def test_props_to_html(self):
+        prop_node = HTMLNode(props={'href': 'api/endpoint', 'target': '_blank', 'data-hidden-val': '300'})
+        expected_str = ' href="api/endpoint" target="_blank" data-hidden-val="300" '
+        self.assertEqual(expected_str, prop_node.props_to_html())
+
+    def test_props_to_html(self):
+        prop_node = HTMLNode()
+        expected_str = ""
+        self.assertEqual(expected_str, prop_node.props_to_html())
+
+if __name__ == '__main__':
+    unittest.main()
